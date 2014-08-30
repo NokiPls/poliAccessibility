8263192af1631c4095b9b47049f3d7a0cce7942c
Nokipls <nokimrn@gmail.com>
2014-08-30 12:44:40 +0200

    robba

--- poliAccessibility/src/main/java/org/springframework/social/quickstart/HomeController.java
+++ poliAccessibility/src/main/java/org/springframework/social/quickstart/HomeController.java
@@ -22,10 +22,13 @@ import com.access.domain.Product;
 @ComponentScan("com.access.services")
 public class HomeController {
 
+	List<Product> products = new ArrayList<Product>();
+
 	// @Autowired
 	public HomeController() {
 	}
 
+	// Home
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String home(Model model) {
 		return "home";
@@ -37,25 +40,55 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/userRegistration", method = RequestMethod.POST)
-	public String commonFriendsList(
+	public String userRegistration(
 			@RequestParam(value = "id[]", required = true) String[] idSelected,
 			Model model) {
 
-		return "commonFriendsList";
+		return "userRegistration";
 	}
 
-	@RequestMapping(value = "/categ", method = RequestMethod.GET)
+	// Le categorie sono hard coded.
+	@RequestMapping(value = "/category", method = RequestMethod.GET)
 	public String categ(Model model) {
-		List<Product> products = new ArrayList<Product>();
-		
+		return "category";
+	}
+
+	// Le sottocategorie sono hard coded.
+	@RequestMapping(value = "/subcategory", method = RequestMethod.GET)
+	public String subcateg(Model model) {
+		return "subcategory";
+	}
+
+	// Prodotti di una categoria
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+	public String products(Model model) {
+
+		products = new ArrayList<Product>();
+
+		// Tirar fuori products dal db
+
+		// Su quante pagine disponiamo n prodotti?
+
 		model.addAttribute("products", products);
-		return "categ";
+		return "products";
+	}
+
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public String product(Model model, @RequestParam(value = "i") int i) {
+
+		// ...
+		model.addAttribute("product", products.get(i));
+		return "product";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
 	}
 	
-	@RequestMapping(value = "/graphNode", method = RequestMethod.GET)
-	public String graphNode(Model model, @RequestParam(value = "id") String id) {
-
-		return "nodePopUp";
+	@RequestMapping(value = "/buy", method = RequestMethod.GET)
+	public String buy(Model model) {
+		return "buy";
 	}
 
 }
